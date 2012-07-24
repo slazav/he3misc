8d0f621b741147be6a3e3d8cc6a02446b11fabdd
Vladislav Zavjalov <slazav@altlinux.org>
2012-07-24 14:48:26 +0400

    update text1

--- text1/texture.f90
+++ text1/texture.f90
@@ -1,6 +1,14 @@
 program texture
   implicit none
 
+  common /texture_pars/ XiH, Lambda, KappaH
+  double precision XiH, Lambda, KappaH
+  data XiH/1D0/    &
+       Lambda/1D0/ &
+       KappaH/1D0/
+
+
+
   write(*,*) 'test'
 
 end
@@ -9,15 +17,106 @@ end
 !  U     -- on input: initial values, on output: result
 !  NU    -- U-function dimensions
 subroutine VAR2D(XMIN,XMAX,YMIN,YMAX,U,NX,NY,NU,H)
+  implicit none
   integer NX,NY,NU, IX,IY,IU
   double precision XMIN,XMAX,YMIN,YMAX
-  double precision U(NX,NY,NU), H
+  double precision U(NX,NY,NU), H, DX,DY
+
+  DX = (XMAX-XMIN)/(NX-1)
+  DY = (YMAX-YMIN)/(NY-1)
 
   !! fix initial values to fit limits and boundary conditions
 
-  !! 
+  !! TODO
+
+  do IU = 1,NU
+    do IX = 1,NX-1
+      do IY = 1,NY-1
+        call SETU(IX, IY, IU)
+      enddo
+    enddo
+  enddo
+
+
   contains
-    function INT_IJ(I, J) result (IIJ)
+
+  !! calculate integral in one cell
+  function INT1(IX, IY)
+    implicit none
+    integer IX,IY, IU
+    double precision INT1
+
+    double precision U_(NU), UX_(NU), UY_(NU)
+    double precision X_, Y_
+
+    ! values in the center of the cell IX..IX+1, IY..IY+1:
+    X_ = XMIN + DX * (IX-0.5D0)
+    Y_ = YMIN + DY * (IY-0.5D0)
+    do IU=1,NU
+      U_(IU)  = ( U(IX,IY,IU) + U(IX+1,IY,IU) + &
+                  U(IX,IY+1,IU) + U(IX+1,IY+1,IU))/4D0
+      UX_(IU) = ( U(IX+1,IY+1,IU) + U(IX+1,IY,IU) &
+                - U(IX,IY+1,IU) - U(IX,IY,IU))/2D0/DX
+      UY_(IU) = ( U(IX+1,IY+1,IU) + U(IX,IY+1,IU) &
+                - U(IX+1,IY,IU) - U(IX,IY,IU))/2D0/DY
+    enddo
+
+    call VAR_FNC(X_, Y_, U_, UX_, UY_, NU, INT1)
+    INT1 = DX*DY*INT1
+  end
+
+  !! calculate integral in 3x3 cell area
+  function INT9(IX, IY)
+    implicit none
+    integer IX,IY, IIX, IIY
+    double precision INT9
+
+    INT9=0
+    do IIX = max(1,IX-1),min(NX,IX+1)
+      do IIY = max(1,IY-1),min(NY,IY+1)
+        INT9 = INT9 + INT1(IIX,IIY)
+      enddo
+    enddo
+  end
+
+  !! calculate and compare integrals in 3x3 cell area for
+  !! -H,0,+H steps and set new value for U
+  subroutine SETU(IX, IY, IU)
+    implicit none
+    integer IX,IY,IU
+
+    double precision S0, SP, SM
+    double precision U_, Umin, Umax
+    double precision X_, Y_
+
+    X_ = XMIN + DX * (IX-0.5D0)
+    Y_ = YMIN + DY * (IY-0.5D0)
+
+    U_ = U(IX, IY, IU) ! save initial value
+    call VAR_LIM(X_,Y_,IU, Umin,Umax) ! get min/max values
+
+    S0 = INT9(IX, IY)
+
+    if (U_ + H.le.Umax) then
+      U(IX, IY, IU) = U_ + H
+      SP = INT9(IX,IY)
+    else
+      SP = S0+1D0 ! something greater than S0
+    endif
+
+    if (SP.lt.S0) return  ! U is already set to U_ + H
+
+    if (U_ - H.ge.Umin) then
+      U(IX, IY, IU) = U_ - H
+      SM = INT9(IX,IY)
+    else
+      SM = S0+1D0
+    endif
+
+    if (SM.lt.S0) return  ! U is already set to U_ - H
+
+    U(IX, IY, IU) = U_
+    return
   end
 end
 
@@ -26,13 +125,17 @@ end
 ! 2D-texture energies in rotating 3He-B
 ! as a function of alpha and beta angles of n-vector
 ! (Gradient energy +  Orientation energy)
-function VAR_FNC(r,z,U,Ur,Uz, NU) result(F)
+subroutine VAR_FNC(r,z,U,Ur,Uz, NU, F)
+  implicit none
   integer NU
-  double precision r,z,U(NU),Ur(NU),Uz(NU)
+  double precision r,z,U(NU),Ur(NU),Uz(NU),F
+
+  common /texture_pars/ XiH, Lambda, KappaH
+  double precision XiH, Lambda, KappaH
 
   double precision CA,CB, SA,SB ! \cos\alpha, \sin\beta, ...
   double precision Nr,Nf,Nz, Nrr,Nfr,Nzr, Nrz,Nfz,Nzz
-  double precision NxZ2, divN, NrotN, Fh, Fg, F
+  double precision NxZ2, divN, NrotN, Fh, Fg
 
   CA = cos(U(1))
   SA = sin(U(1))
@@ -66,26 +169,36 @@ function VAR_FNC(r,z,U,Ur,Uz, NU) result(F)
 end
 
 !!! User defined texture limits and boundary conditions
-subroutine VAR_LIM(r,z,Umin,Umax, NU)
-  integer NU
-  double precision r,z,Umin(NU),Umax(NU)
+subroutine VAR_LIM(r,z,n, Umin,Umax)
+  implicit none
+  integer n
+  double precision r,z,Umin,Umax
 
-  !! limits for \alpha and \beta
-  Umin(1) = -PI
-  Umax(1) = PI
-  Umin(2) = 0D0
-  Umax(2) = 2D0*PI
+  double precision pi
+  data pi/3.14159265358979/
 
-  !! boundary conditions
-  if (r.le.0) then
-    Umin(2) = 0D0
-    Umax(2) = Umin(2)
-  endif
-  if (r.ge.1) then
-    Umin(1) = PI/3D0
-    Umin(2) = Umin(1)
-    Umin(2) = acos(1D0/sqrt(5D0))
-    Umax(2) = Umin(2)
+  !! limits and boundary conditions
+  !! for \alpha (n==1) and \beta (n==2)
+  if (n.eq.1) then
+    Umin = -PI
+    Umax = PI
+    !! boundary conditions:
+    if (r.ge.1D0) then
+      Umin = PI/3D0
+      Umax = Umin
+    endif
+  else
+    Umin = 0D0
+    Umax = 2D0*PI
+    !! boundary conditions
+    if (r.le.0D0) then
+      Umin = 0D0
+      Umax = Umin
+    endif
+    if (r.ge.1D0) then
+      Umin = acos(1D0/sqrt(5D0))
+      Umax = Umin
+    endif
   endif
 end
 

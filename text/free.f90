49d257b354ca1b049fb363d5e049c0dcb6ab9f08
Vladislav Zavjalov <slazav@altlinux.org>
2012-08-22 15:06:31 +0000

    text: cleanup free.f90, move gaussian quadrature from some energies formulas

--- text/free.f90
+++ text/free.f90
@@ -20,6 +20,8 @@ MODULE energies
   REAL (KIND=dp) :: s3 = sqrt(3._dp) ! tmp
   REAL (KIND=dp) :: s5 = sqrt(5._dp) ! tmp
 
+  REAL chia, vd
+
   CONTAINS
 
     SUBROUTINE ab2n(alpha,beta,nz,nr,nf)
@@ -41,7 +43,7 @@ MODULE energies
       END DO
       alpha(0)=x(1)
       beta(0)=0._dp
-      f=energy(alpha,beta)
+      f=energy_int(alpha,beta)
       CALL egrad(alpha,beta,ga,gb)
       DO i=1,nmax
          g(i+1)=ga(i)
@@ -50,66 +52,68 @@ MODULE energies
       g(1)=ga(0)
     END SUBROUTINE sfun
 
+    !!! Textural free energy at given point
+    function energy(alpha,beta, apsi, vz,vr,vf, lz,lr,lf, w) RESULT(e)
+      REAL (KIND=dp) alpha,beta, apsi, vz,vr,vf, lz,lr,lf, w, e
+      REAL (KIND=dp) nz,nr,nf, rzz,rzr,rzf
 
-    FUNCTION energy(alpha,beta) RESULT(e)
-      ! Calculates the textural free energy
+      REAL (KIND=dp) c,s
+      c=-0.25_dp
+      s=SQRT(15.)/4.0_dp
+
+      call ab2n(alpha, beta, nz, nr, nf)
+      rzr=(1-c)*nz*nr-s*nf
+      rzf=(1-c)*nz*nf+s*nr
+      rzz=c+(1-c)*nz**2
+
+      e=0
+      ! magnetic free energy: E = sin(b)**2
+      e = e + sin(beta)**2
+
+      ! spin-orbit free energy
+      ! E = chia*(nub/nu0)^2 (A Psi)^2 sin^2(b)
+      e = e + chia*(nub/nu0 * apsi * sin(beta))**2
+
+      ! flow free energy
+      e = e - 2*(rzr*vr+rzf*vf+rzz*vz)**2/(5*vd**2)
+
+      ! vortex free energy
+      e = e + lo*w*(rzr*lr+rzf*lf+rzz*lz)**2/5
+
+    end
+
+    function energy_int(alpha,beta) RESULT(e)
+      ! Integral of textural free energy
+      ! Gaussian quadrature is used here and below.
       IMPLICIT NONE
       REAL (KIND=dp), DIMENSION(0:nmax) :: alpha,beta
-      REAL (KIND=dp) :: e
-      e=emagn(beta)
-      e=e+spinorbit(beta)
-      e=e+esurf(alpha(nmax),beta(nmax))
-      e=e+ebend(alpha,beta)
-      e=e+eflow(alpha,beta)
-      e=e+evortex(alpha,beta)
-    END FUNCTION energy
-
-
-    FUNCTION emagn(beta) RESULT(e)
-      ! Calculates the magnetic free energy
-      ! Gaussian quadrature is used here and below.
-      ! e = int[ sin(b)**2 ]
-      IMPLICIT NONE
+      REAL (KIND=dp) :: rp,rm,bp,bm,ap,am,apsip,apsim,e
       INTEGER :: i
-      REAL (KIND=dp), DIMENSION(0:nmax) :: beta
-      REAL (KIND=dp) :: rp,rm,bp,bm,e
-      DO i=0,nmax-1
-         rp=(i+sp)*dx
-         rm=(i+sm)*dx
-         bp=sp*beta(i+1)+sm*beta(i)
-         bm=sm*beta(i+1)+sp*beta(i)
-         e = e + dx*(rp*SIN(bp)**2+rm*SIN(bm)**2)*0.5
-      END DO
-    END FUNCTION emagn
 
-
-    FUNCTION spinorbit(beta) RESULT(e)
-      !calculates the spin-orbit free energy
-      ! e = int[ chia*(nub/nu0 * apsi * sin(b))**2 ]
-      IMPLICIT NONE
-      INTEGER :: i
-      REAL (KIND=dp), DIMENSION(0:nmax) :: beta
-      REAL (KIND=dp) :: rp,rm,bp,bm,e,chia,blp,blm,apsip,apsim
       chia=fchia(t,p)
-      e=0.0_dp
-      DO i=0,nmax-1
+      vd=fvd(t,p)
+
+      e=0
+      do i=0,nmax-1
          rp=(i+sp)*dx
          rm=(i+sm)*dx
          bp=sp*beta(i+1)+sm*beta(i)
          bm=sm*beta(i+1)+sp*beta(i)
-        ! blp=ACOS(-1/4+5/4*(COS(bp))**2)
-        ! blm=ACOS(-1/4+5/4*(COS(bm))**2)
+         ap=sp*alpha(i+1)+sm*alpha(i)
+         am=sm*alpha(i+1)+sp*alpha(i)
          apsip=sp*apsi(i+1)+sm*apsi(i)
          apsim=sm*apsi(i+1)+sp*apsi(i)
-         e = e + dx * chia*((nub/nu0)**2) * & 
-           ((apsip**2)*rp*SIN(bp)**2 + (apsim**2)*rm*SIN(bm)**2)*0.5
-      END DO
-    END FUNCTION spinorbit
+         e = e + ( rp*energy(ap,bp,apsip,evzp(i),evrp(i),evfp(i),elzp(i),elrp(i),elfp(i),ewp(i)) + &
+                   rm*energy(am,bm,apsim,evzm(i),evrm(i),evfm(i),elzm(i),elrm(i),elfm(i),ewm(i)) )*0.5*dx
+      enddo
+      e=e+esurf(alpha(nmax),beta(nmax))
+      e=e+ebend(alpha,beta)
+    end function energy_int
 
 
     FUNCTION esurf(alpha,beta) RESULT(e)
       ! Calculates the surface free energy
-      ! e = 
+      ! E = -5/16 (d/aR) (sqrt5 nz*nr - sqrt3 nf)^2 at i=nmax
       IMPLICIT NONE
       INTEGER :: i
       REAL (KIND=dp) :: alpha,beta,dar,nr,nf,nz,e
@@ -119,108 +123,18 @@ MODULE energies
     END FUNCTION esurf
 
 
-    FUNCTION eflow(alpha,beta) RESULT(e)
-      ! Calculates the flow free energy
-      IMPLICIT NONE
-      INTEGER :: i
-      REAL (KIND=dp), DIMENSION(0:nmax) :: alpha,beta
-      REAL (KIND=dp) :: s,c,ap,am,rp,rm,bp,bm,e
-      REAL (KIND=dp) :: vd,nr,nf,nz,rzr,rzf,rzz
-      REAL (KIND=dp) :: vrp,vfp,vzp,vrm,vfm,vzm
-      c=-0.25_dp
-      s=SQRT(15.)/4.0_dp
-      e=0.0_dp
-      vd=fvd(t,p)
-      DO i=0,nmax-1
-         vrp=evrp(i)
-         vfp=evfp(i)
-         vzp=evzp(i)
-         rp=(i+sp)*dx
-         ap=sp*alpha(i+1)+sm*alpha(i)
-         bp=sp*beta(i+1)+sm*beta(i)
-         call ab2n(ap, bp, nz, nr, nf)
-         rzr=(1-c)*nz*nr-s*nf
-         rzf=(1-c)*nz*nf+s*nr
-         rzz=c+(1-c)*nz**2
-         e=e-dx*rp*(rzr*vrp+rzf*vfp+rzz*vzp)**2/(5*vd**2)
-         vrm=evrm(i)
-         vfm=evfm(i)
-         vzm=evzm(i)
-         rm=(i+sm)*dx
-         am=sm*alpha(i+1)+sp*alpha(i)
-         bm=sm*beta(i+1)+sp*beta(i)
-         call ab2n(am, bm, nz, nr, nf)
-         rzr=(1-c)*nz*nr-s*nf
-         rzf=(1-c)*nz*nf+s*nr
-         rzz=c+(1-c)*nz**2
-         e=e-dx*rm*(rzr*vrm+rzf*vfm+rzz*vzm)**2/(5*vd**2)
-      END DO
-    END FUNCTION eflow
-
-
-    FUNCTION evortex(alpha,beta) RESULT(e)
-      ! Calculates the vortex free energy
-      IMPLICIT NONE
-      INTEGER :: i
-      REAL (KIND=dp), DIMENSION(0:nmax) :: alpha,beta
-      REAL (KIND=dp) :: s,c,ap,am,rp,rm,bp,bm,e
-      REAL (KIND=dp) :: wm,wp,nr,nf,nz,rzr,rzf,rzz
-      REAL (KIND=dp) :: lrp,lfp,lzp,lrm,lfm,lzm
-      c=-0.25_dp
-      s=SQRT(15.)/4.0_dp
-      e=0.0_dp
-      DO i=0,nmax-1
-         lrp=elrp(i)
-         lfp=elfp(i)
-         lzp=elzp(i)
-         wp=ewp(i)
-         rp=(i+sp)*dx
-         ap=sp*alpha(i+1)+sm*alpha(i)
-         bp=sp*beta(i+1)+sm*beta(i)
-         call ab2n(ap, bp, nz, nr, nf)
-         rzr=(1-c)*nz*nr-s*nf
-         rzf=(1-c)*nz*nf+s*nr
-         rzz=c+(1-c)*nz**2
-         e=e+dx*lo*rp*wp*(rzr*lrp+rzf*lfp+rzz*lzp)**2/10
-         lrm=elrm(i)
-         lfm=elfm(i)
-         lzm=elzm(i)
-         wm=ewm(i)
-         rm=(i+sm)*dx
-         am=sm*alpha(i+1)+sp*alpha(i)
-         bm=sm*beta(i+1)+sp*beta(i)
-         call ab2n(am, bm, nz, nr, nf)
-         rzr=(1-c)*nz*nr-s*nf
-         rzf=(1-c)*nz*nf+s*nr
-         rzz=c+(1-c)*nz**2
-         e=e+dx*lo*rm*wm*(rzr*lrm+rzf*lfm+rzz*lzm)**2/10
-      END DO
-    END FUNCTION evortex
-
-
     FUNCTION ebend(alpha,beta) RESULT(e)
       ! Calculates the bending free energy
       IMPLICIT NONE
       INTEGER :: i
       REAL (KIND=dp), DIMENSION(0:nmax) :: alpha,beta
-      REAL (KIND=dp) :: da,db,e,con,help,xir,de
+      REAL (KIND=dp) :: da,db,e,con1,con2,help,xir,de
       REAL (KIND=dp) :: ap,am,rp,rm,bp,bm
+      e=0.0_dp
       xir=fxih(t,p,h)/r
       de=fdelta(t,p)
-      e=0.0_dp
-      con=4*(4+de)*xir**2/13
-      DO i=0,nmax-1
-         da=alpha(i+1)-alpha(i)
-         db=beta(i+1)-beta(i)
-         e=e+con*(i+0.5)*db**2
-         rp=(i+sp)
-         rm=(i+sm)
-         bp=sp*beta(i+1)+sm*beta(i)
-         bm=sm*beta(i+1)+sp*beta(i)
-         e=e+0.5*con*da**2*(rp*SIN(bp)**2+rm*SIN(bm)**2)
-         e=e+0.5*con*(SIN(bp)**2/rp+SIN(bm)**2/rm)
-      END DO
-      con=-(2+de)*xir**2/26
+      con1=4*(4+de)*xir**2/13
+      con2=-(2+de)*xir**2/26
       DO i=0,nmax-1
          da=alpha(i+1)-alpha(i)
          db=beta(i+1)-beta(i)
@@ -230,18 +144,21 @@ MODULE energies
          am=sm*alpha(i+1)+sp*alpha(i)
          bp=sp*beta(i+1)+sm*beta(i)
          bm=sm*beta(i+1)+sp*beta(i)
+
+         e=e+con1*(i+0.5)*db**2
+         e=e+0.5*con1*da**2*(rp*SIN(bp)**2+rm*SIN(bm)**2)
+         e=e+0.5*con1*(SIN(bp)**2/rp+SIN(bm)**2/rm)
+
          help=(s5*SIN(ap)-s3*COS(bp)*COS(ap))*db
          help=help+SIN(bp)*(s5*COS(bp)*COS(ap)+s3*SIN(ap))*da
          help=help+SIN(bp)*(s5*COS(bp)*SIN(ap)-s3*COS(ap))/rp
-         e=e+0.5*con*rp*help**2
+         e=e+0.5*con2*rp*help**2
          help=(s5*SIN(am)-s3*COS(bm)*COS(am))*db
          help=help+SIN(bm)*(s5*COS(bm)*COS(am)+s3*SIN(am))*da
          help=help+SIN(bm)*(s5*COS(bm)*SIN(am)-s3*COS(am))/rm
-         e=e+0.5*con*rm*help**2
+         e=e+0.5*con2*rm*help**2
       END DO
-!
       e=e+4*(2+de)*xir**2*SIN(beta(nmax))**2/13
-!
       e=e-2*lsg*xir**2*SIN(beta(nmax))**2/13
     END FUNCTION ebend
 
